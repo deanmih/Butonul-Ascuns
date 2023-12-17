4196c35d16bbccdd3d615e9b8ce430b75a8c6ca0
deanmih <153102912+deanmih@users.noreply.github.com>
2023-12-17 19:59:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let buttonId = ["b1", "b2", "b3"];
+let randomId = buttonId[Math.floor(Math.random() * buttonId.length)];
+let chosenValue;
+//console.log(randomId);
+
+function determineWinnerStatusStandardMode(id) {
+    if (randomId == id) {
+        alert("Congratulations! You picked a winner!");
+    } else {
+        alert("Oh no! Looks like you picked a loser!");
+    }
+}
+
+function generateButtons() {
+    let inputNumber = document.getElementById('num').value;
+    chosenValue = Math.floor(Math.random() * inputNumber);
+    for (let i = 1; i <= inputNumber; ++i) {
+        let btn = document.createElement("button");
+        btn.innerText = "I may just be a winning button!";
+        btn.id = i;
+        document.body.appendChild(btn);
+        btn.addEventListener("click", determineWinnerStatusGeneratedMode);
+    }
+}
+
+function determineWinnerStatusGeneratedMode() {
+    if (this.id == chosenValue) {
+        alert("You won!!");
+    } else {
+        alert("You lost!");
+    }
+}
